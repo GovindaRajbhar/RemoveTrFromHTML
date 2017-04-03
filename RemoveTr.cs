6dba48327f506ecaac309dd24012da68d2dfb4b2
Govinda Rajbhar <rajbhar1993govinda@gmail.com>
2017-04-03 14:13:11 +0530

    Example files related to this topic.

---
+++ RemoveTr.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.IO;
+namespace RemoveTrFromHtmlTemplate.HelperClass
+{
+    public static class RemoveTr
+    {
+        public static void RemoveTrByIdFromHtml()
+        {
+            string filePath = HttpContext.Current.Server.MapPath("~/JobChunkEmailTemplate.html");
+            StringBuilder sbMailTemplate = new StringBuilder();
+
+            using (StreamReader reader = new StreamReader(filePath, Encoding.Default))
+            {
+                sbMailTemplate.Append(reader.ReadToEnd());
+            }
+
+            string pattern = "<tr id=\"GoogleAdwordsSectionId\".+?</tr>";
+            Regex r = new Regex(pattern, RegexOptions.Singleline);
+            string result = r.Replace(sbMailTemplate.ToString(), "");
+            sbMailTemplate.Replace(sbMailTemplate.ToString(), result);
+        }
+    }
+}
